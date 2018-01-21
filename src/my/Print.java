e72f768fa45001a13388bb1f75023617f9c2437b
baijy <0755bai@gmail.com>
2018-01-21 13:26:43 +0800

    convert to maven project

--- src/my/Print.java
+++
@@ -1,8 +0,0 @@
-package my;
-
-public class Print {
-	public static void main(String[] args) {
-		long currentTimeMillis = System.currentTimeMillis();
-		System.out.println(currentTimeMillis);
-	}
-}
