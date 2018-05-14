1c7f9af3ffe426ca3e792596a7588b18c18cad09
柏剑宇 <jianyu.bai@bqjr.cn>
2018-05-14 15:36:56 +0800

    修改测试类

--- src/main/java/com/jianyu/thread/concurrent/CyclicBarrierTest.java
+++ src/main/java/com/jianyu/thread/concurrent/CyclicBarrierTest.java
@@ -1,11 +1,12 @@
 package com.jianyu.thread.concurrent;
 
+import java.util.Random;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 /**
  * CyclicBarrier
- *
+ * 与CountDownLatch的主要区别是多个任务可以相互等待。
  */
 public class CyclicBarrierTest {
     public static void main(String[] args) {
@@ -26,7 +27,7 @@ public class CyclicBarrierTest {
         public void run(){
             System.out.println("线程"+Thread.currentThread().getName()+"正在写入数据...");
             try {
-                Thread.sleep(5000);      //以睡眠来模拟写入数据操作
+                Thread.sleep(1000+ new Random().nextInt(4000));      //以睡眠来模拟写入数据操作
                 System.out.println("线程"+Thread.currentThread().getName()+"写入数据完毕，等待其他线程写入完毕");
                 cyclicBarrier.await();
             } catch (InterruptedException e) {
